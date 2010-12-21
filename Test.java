2f7af0d562201645694a25484a2be1ec5d873cd8
xst <it.does.not@gmail.com>
2010-12-21 14:07:53 -0800

    TEST_00 first commit to git BinaryExpressionTree

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Test_00");
+		System.out.println("first commit to git");
+		System.out.println("BinaryExpressionTree");
+	}
+}
+

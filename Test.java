81552e8944aa9e6b0e680056bec97d57b291c653
xst <it.does.not@gmail.com>
2010-12-21 14:13:02 -0800

    second commit

--- Test.java
+++ Test.java
@@ -2,9 +2,9 @@ public class Test
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Test_00");
-		System.out.println("first commit to git");
-		System.out.println("BinaryExpressionTree");
+		System.out.println("Test_01");
+		System.out.println("second commit to git");
+		System.out.println("BinaryExpressionTree: still unstarted");
 	}
 }
 
